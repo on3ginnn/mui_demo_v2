09f87f02f4fbfe054f9b06354c225dd660cf6ae3
on3gin <on3ginnn@gmail.com>
2025-03-03 21:17:44 +0300

    select area

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { MuiTypography } from './components/MUITypography'
-import { MuiButton } from './components/MUIButton'
-import { MuiTextField } from './components/MUITextField'
-import { Stack } from '@mui/material'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { MuiTypography } from './components/MUITypography';
+import { MuiButton } from './components/MUIButton';
+import { MuiTextField } from './components/MUITextField';
+import { MuiSelect } from './components/MUISelect';
+import { Stack } from '@mui/material';
+import { Container } from '@mui/material';
 
 function App() {
 
   return (
-    <Stack spacing={25}>
-      <MuiTypography></MuiTypography>
-      <MuiButton></MuiButton>
-      <MuiTextField />
-    </Stack>
+    <Container maxWidth='lg'>
+      <Stack spacing={25}>
+        <MuiTypography></MuiTypography>
+        <MuiButton></MuiButton>
+        <MuiTextField />
+        <MuiSelect />
+      </Stack>
+    </Container>
+
   )
 }
 
-export default App
+export default App;
---
+++ src/components/MUISelect.jsx
@@ -0,0 +1,33 @@
+import { Box, TextField, MenuItem } from "@mui/material";
+import { useState } from 'react';
+
+export const MuiSelect = () => {
+    const [countries, setCountries] = useState([]);
+    console.log(countries);
+    const handleChange = (event) => {
+        const value = event.target.value;
+        setCountries(typeof value === "string" ? value.split(',') : value);
+    }
+    return (
+        <Box width="250px">
+            <TextField 
+                label="Select country"
+                select
+                fullWidth
+                value={countries}
+                onChange={handleChange}
+                SelectProps={{
+                    multiple:true
+                }}
+                size='small'
+                color="warning"
+                helperText="Please select your country"
+                error={countries.length == 0}
+            >
+                <MenuItem value="RU">Russia</MenuItem>
+                <MenuItem value="US">USA</MenuItem>
+                <MenuItem value="EU">Europe</MenuItem>
+            </TextField>
+        </Box>
+    )
+}
