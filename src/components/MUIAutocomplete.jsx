d0b992095fff2c249254064d5e46ca4dbf92b603
on3ginnn <124593750+on3ginnn@users.noreply.github.com>
2025-03-10 10:51:18 +0300

    Add files via upload

--- src/components/MUIAutocomplete.jsx
+++ src/components/MUIAutocomplete.jsx
@@ -3,11 +3,21 @@ import { useState } from 'react';
 
 const skills = ["HTML", "CSS", "JavaScript", "TypeScript", "React"];
 
+const skillsOptions = skills.map((skill, index) => ({
+    id: index + 1,
+    label: skill,
+}))
+
 export const MuiAutocomplete = () => {
     const [value, setValue] = useState(null);
+    const [skill, setSkill] = useState(null);
     console.log(value);
+    console.log(skill);
     const handleChange = (event, newValue) => {
         setValue(newValue);
+    }
+    const handleChangeSkillsOptions = (event, newValue) => {
+        setSkill(newValue);
     }
 
     return(
@@ -16,8 +26,10 @@ export const MuiAutocomplete = () => {
             <Box>
                 <Stack spacing={2} width='250px'>
                     <Autocomplete freeSolo value={value} onChange={handleChange} size='small' options={skills} renderInput={(params) => <TextField {...params} label='Skills' />} />
+                    <Autocomplete value={skill} onChange={handleChangeSkillsOptions} size='small' options={skillsOptions} renderInput={(params) => <TextField {...params} label='Skills' />} />
                 </Stack>
             </Box>
+            
         </Stack>
     )
 }
